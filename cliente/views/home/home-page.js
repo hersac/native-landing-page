ed2efb70d46bae8a141832835d961561d2735b7d
Heriberto Sanchez <Heriberto@viumail.com>
2025-05-19 19:18:07 -0500

    Ajusta fondo para seccion de pasaportes, cambia imagenes, ajusta clases

--- cliente/views/home/home-page.js
+++ cliente/views/home/home-page.js
@@ -2,8 +2,8 @@ import { CarouselCard } from "./cards/carouselCard/carousel-card.js";
 
 export class HomePage extends HTMLElement {
   static get observedAttributes() {
-    return ['scroll-position']
-  } 
+    return ["scroll-position"];
+  }
 
   constructor() {
     super();
@@ -30,26 +30,45 @@ export class HomePage extends HTMLElement {
       `;
 
     customElements.define("carousel-card", CarouselCard);
-
-
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'scroll-position') {
+    if (name === "scroll-position") {
       this.scrollPosition = parseFloat(newValue);
+      console.log(this.scrollPosition);
       this.smoothEffect();
     }
   }
 
   smoothEffect() {
-    const contenidoImagen = this.shadowRoot.querySelector('.home__contenido__imagen');
-    const tituloUno = this.shadowRoot.querySelector('.home__contenido__mensaje__titulo__uno');
-    const tituloDos = this.shadowRoot.querySelector('.home__contenido__mensaje__titulo__dos');
-    const mensajeTexto = this.shadowRoot.querySelector('.home__contenido__mensaje__texto');
+    const contenidoImagen = this.shadowRoot.querySelector(
+      ".home__contenido__imagen"
+    );
+    const tituloUno = this.shadowRoot.querySelector(
+      ".home__contenido__mensaje__titulo__uno"
+    );
+    const tituloDos = this.shadowRoot.querySelector(
+      ".home__contenido__mensaje__titulo__dos"
+    );
+    const mensajeTexto = this.shadowRoot.querySelector(
+      ".home__contenido__mensaje__texto"
+    );
 
-    contenidoImagen.style.transform = `translateY(-${this.scrollPosition * 0.08}px)`;
+    contenidoImagen.style.transform = `translateY(-${
+      this.scrollPosition * 0.08
+    }px)`;
     tituloUno.style.transform = `translateY(-${this.scrollPosition * 0.04}px)`;
     tituloDos.style.transform = `translateX(-${this.scrollPosition * 0.02}px)`;
-    mensajeTexto.style.transform = `translateX(${this.scrollPosition * 0.06}px)`;
+    mensajeTexto.style.transform = `translateX(${
+      this.scrollPosition * 0.06
+    }px)`;
+  }
+
+  animatePassport() {
+    const passport = this.shadowRoot.querySelector("#passports");
+
+    if (this.scrollPosition > 400) {
+      passport.style.transform = "aparecer 1s ease-in-out forwards";
+    }
   }
 }
